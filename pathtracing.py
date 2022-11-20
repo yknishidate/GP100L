5369ba4af87fc139f75750d5d0a62b2557fa30de
nishidate-yuki <yuki18425@gmail.com>
2022-11-20 11:01:30 +0900

    simplify

--- pathtracing.py
+++ pathtracing.py
@@ -17,37 +17,31 @@ sphere_radiuses[0] = 0.1
 sphere_radiuses[1] = 100
 sphere_radiuses[2] = 0.2
 sphere_emissions = ti.Vector.field(3, dtype=float, shape=num_spheres)
-sphere_emissions[0] = ti.Vector([1.0, 1.0, 1.0]) * 0.0
-sphere_emissions[1] = ti.Vector([1.0, 1.0, 1.0]) * 0.0
-sphere_emissions[2] = ti.Vector([1.0, 1.0, 1.0]) * 1.0
+sphere_emissions[2] = ti.Vector([1.0, 1.0, 1.0])
 sphere_colors = ti.Vector.field(3, dtype=float, shape=num_spheres)
-sphere_colors[0] = ti.Vector([1.0, 1.0, 1.0]) * 0.5
-sphere_colors[1] = ti.Vector([1.0, 1.0, 1.0]) * 0.9
-sphere_colors[2] = ti.Vector([1.0, 1.0, 1.0]) * 0.0
+sphere_colors[0] = ti.Vector([0.5, 0.5, 0.5])
+sphere_colors[1] = ti.Vector([0.9, 0.9, 0.9])
 
 
 @ti.func
-def intersect(ray_origin, ray_direction, sphere_center, sphere_radius):
-    result = False
-    hit_distance = 0.0
+def intersect(origin, direction, sphere_center, sphere_radius):
+    hit_distance = 10000.0
     hit_position = ti.Vector([0.0, 0.0, 0.0])
     hit_normal = ti.Vector([0.0, 0.0, 0.0])
-    oc = sphere_center - ray_origin
-    b = ti.math.dot(oc, ray_direction)
+    oc = sphere_center - origin
+    b = ti.math.dot(oc, direction)
     det = b * b - ti.math.dot(oc, oc) + sphere_radius * sphere_radius
     if det > 0.0:
         sqrt_det = ti.math.sqrt(det)
-        t1 = b - sqrt_det
-        t2 = b + sqrt_det
+        t1, t2 = b - sqrt_det, b + sqrt_det
         if t1 > eps or t2 > eps:
-            result = True
             if t1 > eps:
                 hit_distance = t1
             else:
                 hit_distance = t2
-            hit_position = ray_origin + hit_distance * ray_direction
+            hit_position = origin + hit_distance * direction
             hit_normal = ti.math.normalize(hit_position - sphere_center)
-    return result, hit_distance, hit_position, hit_normal
+    return hit_distance, hit_position, hit_normal
 
 
 @ti.func
@@ -57,7 +51,6 @@ def sample_direction(normal):
     if ti.abs(w.x) < eps:
         u = ti.math.normalize(ti.math.cross(ti.Vector([1.0, 0.0, 0.0]), w))
     v = ti.math.cross(w, u)
-
     r1 = 2.0 * math.pi * ti.random()
     r2 = ti.random()
     dir = ti.math.normalize(u * ti.cos(r1) * ti.sqrt(r2) +
@@ -79,21 +72,18 @@ def render(frame: int):
             hit_distance = 10000.0
             hit_position = ti.Vector([0.0, 0.0, 0.0])
             hit_normal = ti.Vector([0.0, 0.0, 0.0])
-            hit_emission = ti.Vector([0.8, 0.9, 1.0])
+            hit_emission = ti.Vector([0.8, 0.9, 1.0])  # sky emission
             hit_color = ti.Vector([0.0, 0.0, 0.0])
-            hit_index = -1
             for s in range(num_spheres):
-                result, distance, position, normal = intersect(origin, direction, sphere_centers[s], sphere_radiuses[s])
-                if result:
-                    if distance < hit_distance:
-                        hit_distance = distance
-                        hit_position = position
-                        hit_normal = normal
-                        hit_index = s
-                        hit_emission = sphere_emissions[hit_index]
-                        hit_color = sphere_colors[hit_index]
+                distance, position, normal = intersect(origin, direction, sphere_centers[s], sphere_radiuses[s])
+                if distance < hit_distance:
+                    hit_distance = distance
+                    hit_position = position
+                    hit_normal = normal
+                    hit_emission = sphere_emissions[s]
+                    hit_color = sphere_colors[s]
             color += weight * hit_emission
-            if hit_index == -1:
+            if hit_distance == 10000.0:
                 break
             direction, pdf = sample_direction(hit_normal)
             origin = hit_position + 0.001 * direction
