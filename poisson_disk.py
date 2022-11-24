f35a7043d1dedae97c75a6ab20ec99e7b308bfec
nishidate-yuki <yuki18425@gmail.com>
2022-11-24 13:34:18 +0900

    refactor

--- poisson_disk.py
+++ poisson_disk.py
@@ -2,9 +2,9 @@ import taichi as ti
 
 
 @ti.func
-def exist_neighbors(pos: ti.template(), num_active_particles: int, radius: float) -> bool:
+def exist_neighbors(pos: ti.template(), num_particles: int, radius: float) -> bool:
     found = False
-    for i in range(num_active_particles):
+    for i in range(num_particles):
         p = positions[i]
         vec = p - pos
         dist = ti.sqrt(vec.x * vec.x + vec.y * vec.y)
@@ -14,11 +14,11 @@ def exist_neighbors(pos: ti.template(), num_active_particles: int, radius: float
 
 
 @ti.kernel
-def sample(num_active_particles: int, radius: float):
+def sample(num_particles: int, radius: float):
     for i in range(10):
         pos = ti.Vector([ti.random(), ti.random()])
-        if not exist_neighbors(pos, num_active_particles, radius * 4):
-            positions[num_active_particles] = pos
+        if not exist_neighbors(pos, num_particles, radius * 4):
+            positions[num_particles] = pos
 
 
 if __name__ == '__main__':
@@ -28,14 +28,14 @@ if __name__ == '__main__':
     canvas.set_background_color((1, 1, 1))
 
     radius = 0.01
-    num_particles = 1000
-    num_active_particles = 0
-    positions = ti.Vector.field(2, dtype=float, shape=num_particles)
+    num_particles = 0
+    num_max_particles = 1000
+    positions = ti.Vector.field(2, dtype=float, shape=num_max_particles)
 
     while window.running:
-        if num_active_particles != num_particles:
-            sample(num_active_particles, radius)
-            num_active_particles += 1
+        if num_particles != num_max_particles:
+            sample(num_particles, radius)
+            num_particles += 1
 
         canvas.circles(positions, radius)
         window.show()
