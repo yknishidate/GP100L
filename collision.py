9fce1899cef62adfae2ea474d189daa4aa2a2698
nishidate-yuki <yuki18425@gmail.com>
2024-01-07 01:42:02 +0900

    update

--- collision.py
+++ collision.py
@@ -66,24 +66,24 @@ def intersect_sphere_line_segment(p, q, center, radius):
 
 if __name__ == '__main__':
     ti.init(arch=ti.vulkan)
-    window = ti.ui.Window("Inverse kinematics", (1024, 1024), vsync=True)
+    window = ti.ui.Window("Collision", (1024, 1024), vsync=True)
     canvas = window.get_canvas()
     canvas.set_background_color((1, 1, 1))
 
-    target = ti.Vector.field(2, dtype=float, shape=1)
+    centers = ti.Vector.field(2, dtype=float, shape=1)
 
     gui = window.get_gui()
     rad_a = 0.1
     rad_b = 0.05
     while window.running:
 
-        target[0] = window.get_cursor_pos()
-        canvas.circles(target, rad_a, color=(1.0, 0.0, 0.0))
+        centers[0] = window.get_cursor_pos()
+        canvas.circles(centers, rad_a, color=(1.0, 0.0, 0.0))
 
         color = (0.0, 0.0, 0.0)
         p = ti.Vector([0.3, 0.3])
         q = ti.Vector([0.7, 0.7])
-        if intersect_sphere_line_segment(p, q, target[0], rad_a + rad_b):
+        if intersect_sphere_line_segment(p, q, centers[0], rad_a + rad_b):
             color = (1.0, 0.0, 0.0)
 
         draw_capsule(canvas, p, q,
