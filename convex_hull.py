4d12112ca9f9460dd48c9800fa737e3e51701dc9
nishidate-yuki <yuki18425@gmail.com>
2024-01-17 12:32:54 +0900

    refactor

--- convex_hull.py
+++ convex_hull.py
@@ -2,22 +2,9 @@ import taichi as ti
 import numpy as np
 
 
-def find_furthest_point_in_direction(points, direction):
-    dots = np.dot(points, direction)
-    return np.argmax(dots)
-
-
-def length(vec):
-    return np.linalg.norm(vec)
-
-
-def normalize(vec):
-    return vec / length(vec)
-
-
-def find_furthest_point_from_line(points, line_begin, line_end, direction=None):
-    # 左側を正とする
-    signed_distances = np.cross(line_end - line_begin, points - line_begin)
+def find_furthest_point_from_line(centers, line_begin, line_end, direction=None):
+    # left is positive, right is negative
+    signed_distances = np.cross(line_end - line_begin, centers - line_begin)
     if direction is None:
         return np.argmax(np.abs(signed_distances))
     if direction == "left":
@@ -39,33 +26,20 @@ def sort_ccw(ai, bi, ci):
         return ai, ci, bi
 
 
-def is_inner(p, a, b, c):
-    # abcはCCWとする
-    return np.cross(b - a, p - a) > 0 and \
-        np.cross(c - b, p - b) > 0 and \
-        np.cross(a - c, p - c) > 0
-
-
 def find_hull(centers, convex_hull, convex_hull_lines, a, b):
-    # print(f"find_hull({a}, {b})")
     c = find_furthest_point_from_line(centers, centers[a], centers[b], "right")
     if c is None:
-        # print(f"  c is None")
         convex_hull_lines.append(a)
         convex_hull_lines.append(b)
         return
-    # print(f"  c = {c}")
     convex_hull.append(c)
     find_hull(centers, convex_hull, convex_hull_lines, a, c)
     find_hull(centers, convex_hull, convex_hull_lines, c, b)
 
 
-convex_hull = []
-convex_hull_lines = []
-
 if __name__ == '__main__':
     ti.init(arch=ti.vulkan)
-    window = ti.ui.Window("Collision", (1024, 1024), vsync=True)
+    window = ti.ui.Window("Convex Hull", (1024, 1024), vsync=True)
     canvas = window.get_canvas()
     canvas.set_background_color((1, 1, 1))
 
@@ -81,49 +55,42 @@ if __name__ == '__main__':
     selected = -1
     gui = window.get_gui()
     while window.running:
-        convex_hull = []
-        convex_hull_lines = []
-
-        # Select circle
+        # Select and move circle
         if selected == -1 and window.is_pressed(ti.ui.LMB):
             for i in range(num):
-                if length(window.get_cursor_pos() - centers[i]) < 0.01:
+                if np.linalg.norm(window.get_cursor_pos() - centers[i]) < 0.01:
                     selected = i
                     break
-
         if selected != -1:
             centers[selected] = window.get_cursor_pos()
             if not window.is_pressed(ti.ui.LMB):
                 selected = -1
 
-        # Draw all circles
-        _centers.from_numpy(centers)
-        canvas.circles(_centers, 0.01, color=(0.0, 0.0, 0.0))
-
-        # 2点とる
-        direction = np.array([-1.0, 0.0])
-        index0 = find_furthest_point_in_direction(centers, direction)
-        index1 = find_furthest_point_in_direction(centers, -direction)
-
-        # 線から一番遠い点をとる
-        index2 = find_furthest_point_from_line(centers,
-                                               centers[index0],
-                                               centers[index1])
-
+        index0 = np.argmin(centers[:, 0])  # leftmost
+        index1 = np.argmax(centers[:, 0])  # rightmost
+        index2 = find_furthest_point_from_line(
+            centers, centers[index0], centers[index1])
         index0, index1, index2 = sort_ccw(index0, index1, index2)
-        convex_hull = [index0, index1, index2]
 
+        convex_hull = [index0, index1, index2]
+        convex_hull_lines = []
         find_hull(centers, convex_hull, convex_hull_lines, index0, index1)
         find_hull(centers, convex_hull, convex_hull_lines, index1, index2)
         find_hull(centers, convex_hull, convex_hull_lines, index2, index0)
 
-        for i in range(num):
-            if i in convex_hull:
-                _center[0] = centers[i]
-                canvas.circles(_center, 0.01, color=(1.0, 1.0, 0.0))
+        # Draw all circles
+        _centers.from_numpy(centers.astype(np.float32))
+        canvas.circles(_centers, 0.01, color=(0.0, 0.0, 0.0))
 
-        _indices = ti.field(dtype=int, shape=len(convex_hull_lines))
-        _indices.from_numpy(np.array(convex_hull_lines).astype(np.int32))
-        canvas.lines(_centers, 0.01, indices=_indices)
+        # Draw convex hull lines
+        for i in range(0, len(convex_hull_lines), 2):
+            _line[0] = centers[convex_hull_lines[i]]
+            _line[1] = centers[convex_hull_lines[i + 1]]
+            canvas.lines(_line, 0.01)
+
+        # Draw convex hull circles
+        for i in convex_hull:
+            _center[0] = centers[i]
+            canvas.circles(_center, 0.01, color=(1.0, 0.0, 0.0))
 
         window.show()
