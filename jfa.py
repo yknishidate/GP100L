3750acca16ada3d8544490e57dfc5b5ba2e19751
nishidate-yuki <yuki18425@gmail.com>
2024-02-22 11:21:46 +0900

    add different color

--- jfa.py
+++ jfa.py
@@ -1,20 +1,29 @@
 import taichi as ti
+import numpy as np
+
+
+@ti.func
+def index_color(index: int):
+    r = (index & 1) * 0.5 + 0.5
+    g = ((index >> 1) & 1) * 0.5 + 0.5
+    b = ((index >> 2) & 1) * 0.5 + 0.5
+    return ti.Vector([r, g, b])
 
 
 @ti.kernel
-def render(cell_size: int, grid: ti.template(), colors: ti.template()):
-    for i, j in colors:
+def render(cell_size: int, grid: ti.template(), pixels: ti.template()):
+    for i, j in pixels:
         color = ti.Vector([0.1, 0.1, 0.1])
         x = i // cell_size
         y = j // cell_size
 
         # Grid lines
         if i % cell_size == 0 or j % cell_size == 0:
-            color = ti.Vector([0.5, 0.5, 0.5])
-        elif grid[x, y] == 1:
-            color = ti.Vector([0.1, 0.1, 0.8])
+            color = ti.Vector([0.3, 0.3, 0.3])
+        elif grid[x, y] > 0:
+            color = index_color(grid[x, y])
 
-        colors[i, j] = ti.math.clamp(color, 0.0, 1.0)
+        pixels[i, j] = ti.math.clamp(color, 0.0, 1.0)
 
 
 @ti.kernel
@@ -27,18 +36,22 @@ def jump_flooding_algorithm(grid: ti.template(), step: int):
                 nx, ny = x + dx * step, y + dy * step
                 if nx >= 0 and nx < grid.shape[0] and ny >= 0 and ny < grid.shape[1]:
                     if grid[nx, ny] == 0:
-                        grid[nx, ny] = 1
+                        grid[nx, ny] = grid[x, y]
 
 
 if __name__ == '__main__':
     ti.init(arch=ti.vulkan)
-    width, height = 1024, 1024
+    pixel_res = 1024
     grid_res = 64
-    cell_size = width // grid_res
-    gui = ti.GUI("JFA", res=(width, height), fast_gui=True)
-    colors = ti.Vector.field(3, dtype=float, shape=(width, height))
+    cell_size = pixel_res // grid_res
+    gui = ti.GUI("JFA", res=(pixel_res, pixel_res), fast_gui=True)
+    pixels = ti.Vector.field(3, dtype=float, shape=(pixel_res, pixel_res))
     grid = ti.field(dtype=int, shape=(grid_res, grid_res))
-    grid[8, 8] = 1
+
+    n_points = 5
+    points = np.random.rand(n_points, 2) * grid_res
+    for i in range(n_points):
+        grid[int(points[i, 0]), int(points[i, 1])] = i + 1
 
     step = grid_res // 2
 
@@ -52,6 +65,6 @@ if __name__ == '__main__':
             step //= 2
         frame += 1
 
-        render(cell_size, grid, colors)
-        gui.set_image(colors)
+        render(cell_size, grid, pixels)
+        gui.set_image(pixels)
         gui.show()
