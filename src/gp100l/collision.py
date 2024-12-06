5c5e8a342c8dd0570e6e3a078be497404d1bfca8
Yuki Nishidate <yuki18425@gmail.com>
2024-12-07 00:07:38 +0900

    improve performance

--- src/gp100l/collision.py
+++ src/gp100l/collision.py
@@ -3,10 +3,10 @@ import numpy as np
 
 
 def draw_capsule(canvas, begin, end, radius, color):
-    n = 64
-    centers = ti.Vector.field(2, dtype=float, shape=n)
-    centers.from_numpy(np.linspace(begin, end, n).astype(np.float32))
-    canvas.circles(centers, radius, color=color)
+    capsule_centers[0] = begin
+    capsule_centers[1] = end
+    canvas.circles(capsule_centers, radius, color=color)
+    canvas.lines(capsule_centers, radius * 2, color=color)
 
 
 def intersect_sphere_line(origin, direction, center, radius):
@@ -26,16 +26,16 @@ def intersect_sphere_line(origin, direction, center, radius):
         return True, t
 
 
-def intersect_sphere_segment(sphere_center, sphere_radius, seg_begin, seg_end):
+def intersect_sphere_segment(sphere_center, circle_radius, seg_begin, seg_end):
     distance = np.linalg.norm(seg_end - seg_begin)
     direction = (seg_end - seg_begin) / distance
     intersected, t = intersect_sphere_line(
-        seg_begin, direction, sphere_center, sphere_radius)
+        seg_begin, direction, sphere_center, circle_radius)
     return intersected and 0 <= t <= distance
 
 
-def intersect_sphere_capsule(sphere_center, sphere_rad, cap_begin, cap_end, cap_rad):
-    return intersect_sphere_segment(sphere_center, cap_rad + sphere_rad, cap_begin, cap_end)
+def intersect_sphere_capsule(sphere_center, circle_rad, cap_begin, cap_end, cap_rad):
+    return intersect_sphere_segment(sphere_center, cap_rad + circle_rad, cap_begin, cap_end)
 
 
 if __name__ == '__main__':
@@ -44,21 +44,20 @@ if __name__ == '__main__':
     canvas = window.get_canvas()
     canvas.set_background_color((1, 1, 1))
 
-    centers = ti.Vector.field(2, dtype=float, shape=1)
-
-    gui = window.get_gui()
-    sphere_rad = 0.1
+    circle_rad = 0.1
+    circle_center = ti.Vector.field(2, dtype=float, shape=1)
+    capsule_centers = ti.Vector.field(2, dtype=float, shape=2)
     capsule_rad = 0.05
-    while window.running:
 
-        centers[0] = window.get_cursor_pos()
-        canvas.circles(centers, sphere_rad, color=(1.0, 0.0, 0.0))
+    while window.running:
+        circle_center[0] = window.get_cursor_pos()
+        canvas.circles(circle_center, circle_rad, color=(1.0, 0.0, 0.0))
 
         color = (0.0, 0.0, 0.0)
         cap_begin = np.array([0.3, 0.3])
         cap_end = np.array([0.7, 0.7])
-        if intersect_sphere_capsule(centers[0], sphere_rad, cap_begin, cap_end, capsule_rad):
+        if intersect_sphere_capsule(circle_center[0], circle_rad, cap_begin, cap_end, capsule_rad):
             color = (1.0, 0.0, 0.0)
 
-        draw_capsule(canvas, cap_begin, cap_end, capsule_rad, color=color)
+        draw_capsule(canvas, cap_begin, cap_end, capsule_rad, color)
         window.show()
